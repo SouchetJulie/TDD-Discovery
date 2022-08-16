8e8f436c20ce3304b71c49972d49529b189c7876
Donatien D <ddlu.web@icloud.com>
2022-08-16 15:36:33 +0200

    Specs for fruit_emoji

--- 01-Ruby-basics/00-Lecture/spec/fruit_emoji_spec.rb
+++ 01-Ruby-basics/00-Lecture/spec/fruit_emoji_spec.rb
@@ -1,18 +1,30 @@
-require '../fruit_emoji.rb'
+require 'fruit_emoji'
 
 describe '#fruit_emoji' do
   it 'takes one parameter' do
     expect(method(:fruit_emoji).arity).to eq(1)
   end
 
-  shared_examples 'it works with' do |emoji, argument|
-    it "returns #{emoji} when '#{argument}' is passed as an argument" do
-      expect fruit_emoji(argument).to eq(emoji)
-    end
+  it "returns '🍎' when 'apple' is passed as an argument" do
+    expect(fruit_emoji('apple')).to eq('🍎')
   end
 
-  it_behaves_like 'it works with', '🍎', 'apple'
-  it_behaves_like 'it works with', '🍊', 'orange'
-  it_behaves_like 'it works with', '🍌', 'banana'
-  it_behaves_like 'it works with', '🍑', 'peach'
+  it "returns '🍊' when 'orange' is passed as an argument" do
+    expect(fruit_emoji('orange')).to eq('🍊')
+  end
+
+  it "returns '🍌' when 'banana' is passed as an argument" do
+    expect(fruit_emoji('banana')).to eq('🍌')
+  end
+
+  it "returns '🍑' when 'apple' is passed as an argument" do
+    expect(fruit_emoji('peach')).to eq('🍑')
+  end
+
+  it "returns 'Sorry, I don't know that fruit' with any other input." do
+    fallback_sentence = "Sorry, I don't know that fruit"
+
+    expect(fruit_emoji('')).to eq(fallback_sentence)
+    expect(fruit_emoji('pineapple')).to eq(fallback_sentence)
+  end
 end
