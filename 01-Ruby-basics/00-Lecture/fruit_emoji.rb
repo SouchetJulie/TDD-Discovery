485b6770b664bd6e0b906b8d0aa6b4d4630c8671
Donatien D <ddlu.web@icloud.com>
2022-08-16 14:46:29 +0200

    fruit_emoji — lecture live coding

---
+++ 01-Ruby-basics/00-Lecture/fruit_emoji.rb
@@ -0,0 +1,5 @@
+# TO DO: Write a 'fruit_emoji' method that returns:
+# - 🍎, 🍊, 🍌, 🍑 when those fruits are prompted
+# - The message "Sorry, I don't know that fruit" for other inputs
+
+# ADVANCED: Refactor it with inline conditions
