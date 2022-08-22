14aca8c546d908ad24325d30d6a18f136d4e43ec
Julie SOUCHET <jsouchet@easyvista.com>
2022-08-22 15:13:44 +0000

    exercice 02-02 - roman_style (start)

--- 02-Test-driven-development/02-Roman-style/spec/roman_style_spec.rb
+++ 02-Test-driven-development/02-Roman-style/spec/roman_style_spec.rb
@@ -1,1 +1,14 @@
 # TO DO: Write specs for you roman_style method
+# - I = 1
+# - V = 5
+# - X = 10
+# - L = 50
+# - C = 100
+# - D = 500
+# - M = 1000
+
+require ('roman_style')
+
+describe 'roman_style' do
+  it 'converts simple numbers' do
+    expect()
