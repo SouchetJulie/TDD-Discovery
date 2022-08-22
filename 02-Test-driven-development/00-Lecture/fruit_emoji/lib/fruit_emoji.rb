9ff1b74548a4f2df750ba6e1d8e9e1bcdfd0582f
Donatien D <ddlu.web@icloud.com>
2022-08-22 09:13:18 +0200

    TDD Lecture

---
+++ 02-Test-driven-development/00-Lecture/fruit_emoji/lib/fruit_emoji.rb
@@ -0,0 +1,5 @@
+# TO DO: Write a method which takes a string as an argument and returns:
+# - 🍎, 🍊, 🍌, 🍑 when those fruits are prompted
+# - The message "Sorry, I don't know that fruit" for other inputs
+
+# ADVANCED: Refactor it with inline conditions
