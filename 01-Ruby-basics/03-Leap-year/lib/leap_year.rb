6906ca61869ad04328367292efb29c53c73e6c37
Julie SOUCHET <jsouchet@easyvista.com>
2022-08-22 12:28:54 +0000

    exercice 3 - leap year

--- 01-Ruby-basics/03-Leap-year/lib/leap_year.rb
+++ 01-Ruby-basics/03-Leap-year/lib/leap_year.rb
@@ -1,9 +1,19 @@
-require 'Date'
+require 'date'
 
 def leap?(year)
   # TO DO: Write a method that takes a year and returns if it’s a leap year or not.
-end
+    if Date.leap?(year)
+       puts y.to_s + 'is leap year'
+       return true
+      else  puts 'Argument  is not leap year.'
+        return false
+    end
+  end
+
 
 def big_leap(year)
   # TO DO: Let's improve the method above by adding some text.
-end
+    if Date.leap?(year)
+      else  puts year.to_s + ' is not leap year. The next one is' + (year+1).to_s
+    end
+  end
