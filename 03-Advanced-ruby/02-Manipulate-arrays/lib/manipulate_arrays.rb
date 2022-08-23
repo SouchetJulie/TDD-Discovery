419100c6dbfa8cd0784c99cb4310311ba8c9f2d1
Donatien D <ddlu.web@icloud.com>
2022-08-23 08:14:30 +0200

    Manipulate Arrays exercise

---
+++ 03-Advanced-ruby/02-Manipulate-arrays/lib/manipulate_arrays.rb
@@ -0,0 +1,2 @@
+# TO DO: Write the array_converter, reverse_array, starts_with
+# and starts_with_count methods, according to the specs.
---
+++ 03-Advanced-ruby/02-Manipulate-arrays/spec/manipulate_arrays_spec.rb
@@ -0,0 +1,2 @@
+# TO DO: Write the specs for the array_converter, reverse_array, starts_with
+# and starts_with_count methods.
